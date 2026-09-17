17752a18ecbedd0c04e8698a5e4ecf7d1abcc162
indradoc <indradoc7@gmail.com>
2026-09-17 22:16:50 +0700

    fix homepage route

--- server.mjs
+++ server.mjs
@@ -68,6 +68,9 @@ const productMap = {
 
 app.use(express.json({ limit: '40kb' }));
 app.use(express.static(__dirname));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 /* =========================================================
    DATABASE
